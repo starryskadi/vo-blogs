9a10992f8539e5e05e2bbdf80b244ba8cc88e99f
starryskadi <starryskadi@users.noreply.github.com>
2019-02-18 22:21:47 +0630

    Updates

---
+++ precache-manifest.82db3ece4486ca548f23b019b6f27475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b7b9b5168f4cba96af",
+    "url": "/vo-blogs/static/css/main.8f9b7f63.chunk.css"
+  },
+  {
+    "revision": "12b7b9b5168f4cba96af",
+    "url": "/vo-blogs/static/js/main.12b7b9b5.chunk.js"
+  },
+  {
+    "revision": "d63245467e0f4883f5a3",
+    "url": "/vo-blogs/static/js/runtime~main.d6324546.js"
+  },
+  {
+    "revision": "fe1aee38ec937c9f5652",
+    "url": "/vo-blogs/static/js/2.fe1aee38.chunk.js"
+  },
+  {
+    "revision": "67307f21a75acc79de4fd6d8919ac605",
+    "url": "/vo-blogs/index.html"
+  }
+];
